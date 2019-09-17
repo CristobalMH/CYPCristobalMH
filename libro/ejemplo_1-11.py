3e14ea844a5b3248d289d70c1a8af41896781438
CristobalMH <rawrr.rasmus@gmail.com>
2019-09-17 16:56:18 -0500

    ejemplo_1-11.py

---
+++ libro/ejemplo_1-11.py
@@ -0,0 +1,8 @@
+#Ingresa digitos enteros para A,B,C y D y los imprime en orden inverso
+print("Ingrese 4 numeros enteros:")
+A = int(input("A = "))
+B = int(input("B = "))
+C = int(input("C = "))
+D = int(input("D = "))
+
+print(D,C,B,A)
