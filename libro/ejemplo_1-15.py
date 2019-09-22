825d90cc992d706e1d83258e37e55935c4bbbb08
CristobalMH <rawrr.rasmus@gmail.com>
2019-09-22 09:05:30 -0500

    Perímetro_Superficie_Rectangulo

---
+++ libro/ejemplo_1-15.py
@@ -0,0 +1,9 @@
+print("Introduzca la base y la altura de un rectangulo para obtener la superficie y perímetro")
+BASE = float(input("BASE = "))
+ALTURA = float(input("ALTURA = "))
+
+SUP = BASE * ALTURA
+PER = 2 * (BASE + ALTURA)
+
+print("La superficie de la figura es de", SUP, "unidades cuadradas")
+print("El perimetro de la figura es de", PER, "unidades")
