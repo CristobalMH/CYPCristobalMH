5c4e23a895f61509466e3dbbc99847d4b90eac52
CristobalMH <rawrr.rasmus@gmail.com>
2019-09-22 08:39:27 -0500

    Promedio_Calificación

---
+++ libro/ejemplo_1-13.py
@@ -0,0 +1,13 @@
+MAT = int(input("Numero de cuenta: "))
+
+print("Introduzca 5 calificaciones: ")
+
+calif1 = int(input("calif_1: "))
+calif2 = int(input("calif_2: "))
+calif3 = int(input("calif_3: "))
+calif4 = int(input("calif_4: "))
+calif5 = int(input("calif_5: "))
+
+PROM = (calif1 + calif2 + calif3 + calif4 + calif5)/5
+
+print("El Alumno con el número de cuenta", MAT, "tiene un promedio de", PROM)
