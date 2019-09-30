60afe672b55eef369d959e04608514682db82616
CristobalMH <48938709+CristobalMH@users.noreply.github.com>
2019-09-30 17:48:32 -0500

    Delete problema2_2.py

--- libro/problemas_resueltos/capitulo_2/problema2_2.py
+++
@@ -1,9 +0,0 @@
-SUE = float(input("Ingrese su saldo actual: "))
-
-if SUE < 1000:
-	AUM = SUE * 0.15
-	NSUE = SUE + AUM
-	print("Tiene un aumento de $ %.2f" %(AUM))
-	print("Su nuevo sueldo es de $ %.2f" %(NSUE))
-else:
-print("Su saldo sigue siendo de $ %.2f" %(SUE))
