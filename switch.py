99f3b198c78519a975031d33a0b2c31d89f4740c
CristobalMH <rawrr.rasmus@gmail.com>
2019-10-08 17:39:08 -0500

    Switch

---
+++ switch.py
@@ -0,0 +1,16 @@
+DIA = int(input("Introduce un día (1-5): "))
+
+if DIA == 1:
+	print("Lunes")
+elif DIA == 2:
+	print("Martes")
+elif DIA == 3:
+	print("Miercoles")
+elif DIA == 4:
+	print("Jueves")
+elif DIA == 5:
+	print("Viernes")
+else:
+	print("Opción No Válida")
+
+print("Fin del Programa")
