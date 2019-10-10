89c45af1dab337d17929b2df11245bba71764324
CristobalMH <rawrr.rasmus@gmail.com>
2019-10-10 16:57:24 -0500

    Positivo_Negativo_Nulo

---
+++ libro/problemas_resueltos/capitulo_2/problema2_5.py
@@ -0,0 +1,9 @@
+NUM = int(input("Ingrese cualquier número que desee: "))
+
+if NUM > 0:
+	print("%d Es Positivo" %(NUM))
+elif NUM == 0:
+	print("%d Es Nulo" %(NUM))
+else:
+	print("%d Es Negativo" %(NUM))
+
